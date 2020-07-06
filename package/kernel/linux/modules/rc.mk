b8209851f45d174f9ffa50cff1d78db178f24d43
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-07-07 00:11:21 +0200

    Support Remote Controllers

---
+++ package/kernel/linux/modules/rc.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2020 Rafostar
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+RC_MENU:=Remote Controller Support
+
+define KernelPackage/rc-core
+  SUBMENU:=$(RC_MENU)
+  TITLE:=Support for Remote Controllers
+  DEPENDS:=+kmod-input-core
+  KCONFIG:=CONFIG_RC_CORE
+  FILES:=$(LINUX_DIR)/drivers/media/rc/rc-core.ko
+  AUTOLOAD:=$(call AutoLoad,50,rc-core)
+endef
+
+define KernelPackage/rc-core/description
+  Enable support for Remote Controllers on Linux. This is
+  needed in order to support several video capture adapters,
+  standalone IR receivers/transmitters, and RF receivers.
+
+  Enable this option if you have a video capture board even
+  if you don't need IR, as otherwise, you may not be able to
+  compile the driver for your adapter.
+endef
+
+$(eval $(call KernelPackage,rc-core))
